c937dcd80e1b9bcc8e55ccc1aa6a61f6aa652513
abhishekjakhar <abhishekjakhar.11@gmail.com>
2020-04-22 10:59:45 +0530

    Meta component added + website.js config added

---
+++ components/Meta.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import { siteTitle, siteDescription } from '../config/website'
+
+const Meta = () => (
+  <Head>
+    <meta charSet="utf-8" />
+    <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <title>{siteTitle}</title>
+
+    <meta name="title" content={siteTitleAlt} />
+    <meta name="description" content={siteDescription} />
+    <meta name="robots" content="index,follow" />
+  </Head>
+)
+
+export default Meta
---
+++ config/website.js
@@ -0,0 +1,18 @@
+export const siteTitle = 'Abhishek Jakhar' // Navigation and Site Title
+export const siteTitleAlt = 'The personal website of Abhishek Jakhar' // Alternative Site title for SEO
+export const siteTitleShort = 'abhishekjakhar' // short_name for manifest
+export const siteUrl = process.env.ROOT_URL || 'https://abhihekjakhar.com' // Domain of your site. No trailing slash!
+export const lang = 'en' // Language Tag on <html> element
+export const pathPrefix = '/'
+export const siteLogo = 'images/logo.png' // Used for SEO and manifest, path to your image you placed in the 'static' folder
+export const siteDescription =
+  "Hello, I'm Abhishek Jakhar, a Front-End Developer &amp; Experience Designer."
+export const author = 'Abhishek Jakhar'
+export const minibio =
+  'I am a Front-End developer &amp; UX designer, because I am intrinsically a problem solver. I believe that any problem can be solved through design thinking and when it comes to code I believe everything is possible with added complexity.'
+// Social link
+export const socialLinks = {
+  medium: 'https://medium.com/@abhishekjakhar',
+  github: 'https://github.com/abhishekjakhar',
+  linkedin: 'https://www.linkedin.com/in/abhishekjakhar',
+}
