6f1ad2d4f563c916cf7cafccd1229a4a3f8a9d4f
Abhishek Jakhar <abhishekjakhar.11@gmail.com>
2021-01-03 15:42:42 +0530

    chore: remove Google analytics, use Vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,5 @@
 import '../styles/index.css'
-import Router from 'next/router'
 import ProgressBar from '../components/ProgressBar'
-
-import * as gtag from '../lib/gtag'
-
-Router.events.on('routeChangeComplete', url => gtag.pageView(url))
 
 export default function MyApp({ Component, pageProps }) {
   return (
--- pages/_document.js
+++ pages/_document.js
@@ -1,31 +1,10 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Main, NextScript } from 'next/document'
 import { lang } from '../config/website'
-import { GA_TRACKING_ID } from '../lib/gtag'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang={lang}>
-        <Head>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', '${GA_TRACKING_ID}', {
-                  page_path: window.location.pathname
-                });
-              `,
-            }}
-          />
-        </Head>
         <body className="light">
           <Main />
           <NextScript />
